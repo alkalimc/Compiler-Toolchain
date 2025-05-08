c6eba83f0d8a24e609b955fe9f3f98d18b83b27e
alkalimc <173126680+alkalimc@users.noreply.github.com>
2025-05-08 09:31:16 +0800

    fix GPTQ Evaluation

--- CT/Evaluation/GPTQ/lmEvaluationHarness.py
+++ CT/Evaluation/GPTQ/lmEvaluationHarness.py
@@ -11,50 +11,28 @@ from gptqmodel import QuantizeConfig
 
 @dataclass
 class LmEvaluationHarness():
-    '''更换数据生成逻辑，一部分变量放到postinit动态生成
     username: str = field(default="Compiler-Toolchain")
-    workspace: str = field(default=f"/data/disk0/Workspace/{username}")
-
-    model_id: str = field(default="Qwen2.5-7B-Instruct")
-    model_path: str = field(default=f"{workspace}/Models/{model_id}")
-
-    evaluation_path: str = field(default=f"{workspace}/Evaluations/Quanted/{model_id}")
-    evaluation_framework = EVAL.LM_EVAL
-    evaluation_tasks: EVAL.LM_EVAL = field(default=EVAL.LM_EVAL.ARC_EASY)
-    evaluation_batch_size: int = field(default=1, metadata={"min_value": 1})
-    evaluation_output_path: str = field(default=f"{evaluation_path}/{evaluation_framework.name}/{model_id}{evaluation_framework.name}{evaluation_tasks.name}.json")
-
-    trust_remote_code: bool = field(default=True)
-    '''
-    ###
-    username: str = field(default="Compiler-Toolchain")
-    model_id: str = field(default="Qwen2.5-7B-Instruct")
-    evaluation_tasks: EVAL.LM_EVAL = field(default=EVAL.LM_EVAL.ARC_CHALLENGE)
+    model_id: str = field(default="Qwen2.5-7B-Instruct-W4A16-GPTQ")
     evaluation_batch_size: int = field(default=1)
+
+    evaluation_path: str = field(init=False)
+    evaluation_framework: EVAL = field(init=False, default=EVAL.LM_EVAL)
+    evaluation_task: EVAL.LM_EVAL = field(default=EVAL.LM_EVAL.ARC_CHALLENGE)
+    evaluation_batch_size: int = field(default=4, metadata={"min_value": 1})
+    evaluation_output_path: str = field(init=False)
+
     trust_remote_code: bool = field(default=True)
 
-    workspace: str = field(init=False)
-    model_path: str = field(init=False)
-    evaluation_path: str = field(init=False)
-    evaluation_output_path: str = field(init=False)
-    evaluation_framework: EVAL = field(init=False, default=EVAL.LM_EVAL)
-
-    
     def __post_init__(self):
-        ###
-        self.workspace = f"/data/disk0/Workspace/{self.username}"
-        self.model_path = f"{self.workspace}/Models/{self.model_id}"
-        self.evaluation_path = f"{self.workspace}/Evaluations/Quanted/{self.model_id}"
-        self.evaluation_output_path = (
-            f"{self.evaluation_path}/{self.evaluation_framework}/"
-            f"{self.model_id}{self.evaluation_framework}{self.evaluation_tasks}.json"
-        )
+        workspace: str = os.path.join("/data/disk0/Workspace", self.username)
+        model_path: str = os.path.join(workspace, "Models", "Quanted", self.model_id)
+        evaluation_path: str = os.path.join(workspace, "Evaluations", "Quanted", self.model_id, self.evaluation_framework, f"{self.evaluation_tasks}.json")
 
         evalplus_results = GPTQModel.eval(
-            model_or_id_or_path=self.model_path,
+            model_or_id_or_path=model_path,
             framework=self.evaluation_framework,
-            tasks=self.evaluation_tasks,
+            tasks=self.evaluation_task,
             batch_size=self.evaluation_batch_size,
             trust_remote_code=self.trust_remote_code,
-            output_path=self.evaluation_output_path
+            output_path=self.evaluation_path
             )
